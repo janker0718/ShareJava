0180f0b4f5da77e845117805bc20f2df17b10439
janker0718 <690864766@qq.com@qq.com>
2023-01-15 00:43:27 +0800

    优化meta数据

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -8,7 +8,29 @@ export default defineUserConfig({
   dest: "docs/.vuepress/.dist",
   title: "爪哇干货分享",
   description: "包含: Java 基础，面试小册，Spring源码解析，中间件原理",
-  head: [['script', {}, `
+  head: [
+    // meta
+    ["meta", { name: "robots", content: "all" }],
+    ["meta", { name: "author", content: "Janker" }],
+    [
+      "meta",
+      {
+        "http-equiv": "Cache-Control",
+        content: "no-cache, no-store, must-revalidate",
+      },
+    ],
+    ["meta", { "http-equiv": "Pragma", content: "no-cache" }],
+    ["meta", { "http-equiv": "Expires", content: "0" }],
+    [
+      "meta",
+      {
+        name: "keywords",
+        content:
+            "Java基础, 多线程, JVM, 虚拟机, 数据库, MySQL, Spring, Redis, MyBatis, 系统设计, 分布式, RPC, 高可用, 高并发, Dubbo, Seata",
+      },
+    ],
+    ["meta", { name: "apple-mobile-web-app-capable", content: "yes" }],
+      ['script', {}, `
       var _hmt = _hmt || [];
       (function() {
         var hm = document.createElement("script");
