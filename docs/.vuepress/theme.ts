bc524b59e1f1595790341ed8f2f5792dfb0a82fb
janker0718 <690864766@qq.com@qq.com>
2023-03-05 21:57:05 +0800

    # 完善 web 开发参考指南

--- docs/.vuepress/theme.ts
+++ docs/.vuepress/theme.ts
@@ -52,7 +52,7 @@ export default hopeTheme({
     },
   },
   plugins: {
-    blog: true,
+    blog: false,
     // If you don’t need comment feature, you can remove following option
     // The following config is for demo ONLY, if you need comment feature, please generate and use your own config, see comment plugin documentation for details.
     // To avoid disturbing the theme developer and consuming his resources, please DO NOT use the following config directly in your production environment!!!!!
